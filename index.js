7c6354a646c71e0e51596e2d4c8f076901500a8f
Jobsidney <jobsidney67@gmail.com>
2022-06-02 11:38:22 +0300

    Adding push unshift methods

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 const superheroes = ["Catwoman", "Storm", "Jessica Jones"];
+
+//adding item at end of Array
+function addItem(name){
+    superheroes.push(name);
+    console.log(superheroes);
+}
+
+//Adding item at beggining of Array
+function addingItemBegin(name){
+    superheroes.unshift(name);
+    console.log(superheroes);
+}
+//Creating copy of an ARRAY
+const newArray=[...superheroes];
+console.log(newArray);
+
+
+//calling the functions
+addItem("SuperMan");
+addingItemBegin("SpiderMan");
